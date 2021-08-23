e9d02133775a854c484e3d44667401f97588a86c
Илья Ирхин <i.irhin@yandex.ru>
2021-08-23 08:49:40 +0300

    little changing

--- DynamicStaticPolymorphism/Program.cs
+++ DynamicStaticPolymorphism/Program.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 /// В случае если нам нужно как-то модернизировать, расширить класс Product (Допустим поменять логику метода Discount) для определённой категории товаров,например: 
 /// мы продаём молочный продукты и хотим сделать для них отдельно определённую скидку допустим понижать цену на 100 рубасов.
 /// то мы можем модернизировать, расширить по SOLID(2ой принцип) класс Product c помощью так-называемого ПОЛИМОРФИЗМА ЁПТА
-/// для этого используется динамический полиморфизм , когда создаётся виртуальный метод , который в будущем для производных классов будем переопределять
+/// для этого используется динамический полиморфизм , когда создаётся виртуальный метод , который в будущем для производных классов будем переопределять виртуальный метод
 /// статический полиморфизм также помогает расширить базовый класс, но это не является полиморфизмом по своей сути - т.к просто реализуется перезагрузка, замещение/скрытие базого, невиртуального метода.
 /// Также отличие статического полиморфизма от динамического, в том что в статическом - на этапе компиляции, определяется какой будет вызываться метод в родительском классе и его производном.
 /// В динамическом же полиморфизме непонятно какой будет вызываться метод в родительском классе - переопределённый(override) метод или базовый метод(virtual).Это определяется во время выполнения программы.
@@ -32,16 +32,16 @@ namespace DynamicStaticPolymorphism
         {
             Price = Price * (100 - percent) / 100;
 
-            Name += " classProduct";
+            Name += " class Product";
         }
 
     }
     //1ый тип полиморфизма - динамический , когда Product не понимает еще какой метод он будет вызывать virtual или override
-    // Product s = new Dildo(); - вызовется переопределённый метод , т.к после приведения произовдного типа Dildo к базовому Product ссылка Product s будет ссылаться на метод Dildo.Discount().
-    // Dildo s = new Dildo(); - вызовется переопределённый метод
-    class Dildo : Product
+    // Product s = new Milk(); - вызовется переопределённый метод , т.к после приведения произовдного типа Milk к базовому Product ссылка Product s будет ссылаться на метод Milk.Discount().
+    // Milk s = new Milk(); - вызовется переопределённый метод
+    class Milk : Product
     {
-        public Dildo(string name, int price): base(name, price)
+        public Milk(string name, int price): base(name, price)
         {
             this.Name = name;
             this.Price = price;
@@ -51,18 +51,18 @@ namespace DynamicStaticPolymorphism
             base.Discount(percent);
             this.Price -= 55;
 
-            Name += " classDildo";
+            Name += " class Milk";
         }
     }
     //2ой тип полиморфизма - статический или еще как можно назвать замещением по урокам ITDVN, но в случае, если у методов совпадают сигнатуры.
     //Но по своей сути это является перегрузкой метода,  и можно вызвать как производный метод так и базовый метод ,если сигнатуры различаются.
-    // Product s = new Smazka(); - вызовется базовый метод, т.к после приведения производного класса Preziki к базовому Product - метод Preziki.Discount() заместится на Product.Discount() 
-    // ,все потому что s будет ссылаться на класс Product, соответственно т.к в Preziki метод не переопределен, а просто перегружен ,то метод Preziki.Discount ,просто будет не виден.
-    // Smazka s = new Smazka(); - вызовется производный метод Product.Discount().
-    // Smazka s = new Product(); - ну тут тебя нахер компилятор пошлёт с такими запросами) 
-    class Smazka : Product
+    // Product s = new Drinks(); - вызовется базовый метод, т.к после приведения производного класса Household к базовому Product - метод Household.Discount() заместится на Product.Discount() 
+    // ,все потому что s будет ссылаться на класс Product, соответственно т.к в Household метод не переопределен, а просто перегружен ,то метод Household.Discount ,просто будет не виден.
+    // Drinks s = new Drinks(); - вызовется производный метод Product.Discount().
+    // Drinks s = new Product(); - ну тут тебя нахер компилятор пошлёт с такими запросами) 
+    class Drinks : Product
     {
-        public Smazka(string name, int price) : base(name, price)
+        public Drinks(string name, int price) : base(name, price)
         {
             this.Name = name;
             this.Price = price;
@@ -72,7 +72,7 @@ namespace DynamicStaticPolymorphism
             base.Discount(percent);
             this.Price -= 77;
 
-            Name += " classSmazka";
+            Name += " class Drinks";
         }
 
         public void Discount()
@@ -80,20 +80,20 @@ namespace DynamicStaticPolymorphism
           
             this.Price -= 90;
 
-            Name += " classPreziki";
+            Name += " class Household";
         }
     }
 
     //2ой тип полиморфизма(аналогичный только для удобства исп ключ слово new) - статический или еще как можно назвать замещением по урокам ITDVN, но в случае, если у методов совпадают сигнатуры. 
     //Но по своей сути это является перегрузкой метода,  и можно вызвать как производный метод так и базовый метод ,если сигнатуры различаются.
     //В зависимости от того на какой класс ссылается объект, такой и вызывается метод.
-    // Product s = new Preziki(); - вызовется базовый метод, т.к после приведения производного класса Preziki к базовому Product - метод Preziki.Discount() заместится на Product.Discount() 
-    // ,все потому что s будет ссылаться на класс Product, соответственно т.к в Preziki метод не переопределен, а просто перегружен ,то метод Preziki.Discount ,просто будет не виден.
-    // Preziki s = new Preziki(); - вызовется производный метод Product.Discount().
-    // Preziki s = new Product(); - ну тут тебя нахер компилятор пошлёт с такими запросами) 
-    class Preziki : Product
+    // Product s = new Household(); - вызовется базовый метод, т.к после приведения производного класса Household к базовому Product - метод Household.Discount() заместится на Product.Discount() 
+    // ,все потому что s будет ссылаться на класс Product, соответственно т.к в Household метод не переопределен, а просто перегружен ,то метод Household.Discount ,просто будет не виден.
+    // Household s = new Household(); - вызовется производный метод Product.Discount().
+    // Household s = new Product(); - ну тут тебя нахер компилятор пошлёт с такими запросами) 
+    class Household : Product
     {
-        public Preziki(string name, int price) : base(name, price)
+        public Household(string name, int price) : base(name, price)
         {
             this.Name = name;
             this.Price = price;
@@ -104,7 +104,7 @@ namespace DynamicStaticPolymorphism
             base.Discount(percent);
             this.Price -= 100;
 
-            Name += " classPreziki"; 
+            Name += " class Household"; 
         }
     }
 
@@ -112,10 +112,10 @@ namespace DynamicStaticPolymorphism
     {
         static void Main(string[] args)
         {
-            Dildo d = new Dildo("DildoForever", 1000);
-            Smazka s = new Smazka("SmazkaTeplaya", 1000);
-            Preziki p = new Preziki("100%, no 99.9%", 1000);
-            
+            Milk d = new Milk("Milk vologodskoye", 1000);
+            Drinks s= new Drinks("Coca-cola", 1000);
+            Household p= new Household("Sponges Ultra", 1000);
+
             d.Discount(10);
             s.Discount(10);
             p.Discount(10);
@@ -125,9 +125,9 @@ namespace DynamicStaticPolymorphism
             Console.WriteLine($"Name {p.Name}, Price: {p.Price}");
 
             /////////////////////////////////////////////////////////////
-            d = new Dildo("DildoForever", 1000);
-            s = new Smazka("SmazkaTeplaya", 1000);
-            p = new Preziki("100%, no 99.9%", 1000);
+            d = new Milk("Milk vologodskoye", 1000);
+            s = new Drinks("Coca-cola", 1000);
+            p = new Household("Sponges Ultra", 1000);
 
            
             List<Product> list = new List<Product>() { new Product("Tvorog", 1000), d, s, p };
